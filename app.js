d3327061467c00045b98a1b1a705604a33c7418b
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-12-23 20:18:45 +0300

    add images to graph nodes

--- app.js
+++ app.js
@@ -18,6 +18,9 @@ var graph = {
     multigraph: false
 };
 
+var width = 900;
+var height = 900;
+
 var getFriends = function(id, callback) {
     console.log('Loading friends for ' + id + '...');
 
@@ -49,50 +52,56 @@ var addFriendToGraph = function(userIndex, friend) {
 };
 
 var draw = function() {
-    var simulation = d3.forceSimulation()
-        .force("link", d3.forceLink().id(function(d) { return d.id; }))
-        .force("charge", d3.forceManyBody())
-        .force("center", d3.forceCenter());
+    var svg = d3.select('body').append('svg')
+        .attr('width', width)
+        .attr('height', height);
 
-    var canvas = document.querySelector("canvas"),
-        context = canvas.getContext("2d"),
-        width = canvas.width,
-        height = canvas.height;
+    var simulation = d3.forceSimulation(graph.nodes)
+        .force('link', d3.forceLink(graph.links).id(function(d) { return d.id; }))
+        .force('charge', d3.forceManyBody().strength(-800).distanceMax(1000))
+        .force('center', d3.forceCenter().x(450).y(450))
+        .on('tick', tick);
 
-    simulation
-        .nodes(graph.nodes)
-        .on("tick", ticked);
 
-    simulation.force("link")
-        .links(graph.links);
+    var links = svg.selectAll('line')
+        .data(graph.links)
+        .enter().append('line')
+        .style('stroke', '#ccc')
+        .style('stroke-width', 1);
 
-    function ticked() {
-        context.clearRect(0, 0, width, height);
-        context.save();
-        context.translate(width / 2, height / 2);
+    var nodes = svg.selectAll('g.node')
+        .data(graph.nodes)
+        .enter().append('g');
 
-        context.beginPath();
-        graph.links.forEach(drawLink);
-        context.strokeStyle = "#aaa";
-        context.stroke();
+    nodes.append('circle')
+        .attr('r', 10);
 
-        context.beginPath();
-        graph.nodes.forEach(drawNode);
-        context.fill();
-        context.strokeStyle = "#fff";
-        context.stroke();
+    var clipPath = nodes.append('clipPath').attr('id', 'clipCircle');
+    clipPath.append('circle')
+        .attr('r', 20);
 
-        context.restore();
-    }
+    var images = nodes.append('svg:image')
+        .attr('xlink:href', function(d) { return d.photo;})
+        .attr('x', function(d) { return -20;})
+        .attr('y', function(d) { return -20;})
+        .attr('height', 40)
+        .attr('width', 40)
+        .attr('clip-path', 'url(#clipCircle)');
 
-    function drawLink(d) {
-        context.moveTo(d.source.x, d.source.y);
-        context.lineTo(d.target.x, d.target.y);
-    }
+    function tick() {
+        links.attr('x1', function(d) { return d.source.x; })
+            .attr('y1', function(d) { return d.source.y; })
+            .attr('x2', function(d) { return d.target.x; })
+            .attr('y2', function(d) { return d.target.y; });
 
-    function drawNode(d) {
-        context.moveTo(d.x + 3, d.y);
-        context.arc(d.x, d.y, 3, 0, 2 * Math.PI);
+        nodes.attr('transform', function(d) {
+            return 'translate(' + d.x + ',' + d.y + ')';
+        });
+
+        nodes.attr('x', function(d) {return d.x = Math.max(16, Math.min(width - 16, d.x));})
+            .attr('y', function(d) {return d.y = Math.max(16, Math.min(height - 16, d.y));});
+
+        simulation.alpha(0.4);
     }
 };
 
