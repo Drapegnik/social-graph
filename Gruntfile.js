0cfe69fdefc8956fc3cebc8038ab6b7c59f67858
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2016-12-29 13:50:23 +0300

    switch styles to sass, add bootswatch themes

---
+++ Gruntfile.js
@@ -0,0 +1,111 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    // Automatically load required Grunt tasks
+    require('jit-grunt')(grunt);
+
+    // Project configuration.
+    grunt.initConfig({
+        // Empties folders to start fresh
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: ['app/styles/*.css']
+                }]
+            },
+            server: 'app'
+        },
+
+        // Add vendor prefixed styles
+        postcss: {
+            options: {
+                map: true,
+                processors: [
+                    require('autoprefixer')
+                ]
+            },
+            server: {
+                options: {
+                    map: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'app/styles/',
+                    src: '{,*/}*.css',
+                    dest: 'app/styles/'
+                }]
+            },
+            dist: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/styles/',
+                    src: '{,*/}*.css',
+                    dest: 'app/styles/'
+                }]
+            }
+        },
+
+        // Compiles Sass to CSS and generates necessary files if requested
+        compass: {
+            options: {
+                sassDir: 'app/styles',
+                cssDir: 'app/styles',
+                importPath: './node_modules',
+                relativeAssets: false,
+                assetCacheBuster: false,
+                raw: 'Sass::Script::Number.precision = 10\n'
+            },
+            dist: {
+                options: {
+                }
+            },
+            server: {
+                options: {
+                    sourcemap: true
+                }
+            }
+        },
+
+        // minimize .css
+        cssmin: {
+            target: {
+                files: [{
+                    expand: true,
+                    cwd: 'app/styles',
+                    src: ['*.css', '!*.min.css'],
+                    dest: 'app/styles',
+                    ext: '.min.css'
+                }]
+            }
+        },
+
+        // Run some tasks in parallel to speed up the build process
+        concurrent: {
+            server: [
+                'compass:server'
+            ],
+            test: [
+                'compass'
+            ],
+            dist: [
+                'compass:dist'
+            ]
+        }
+
+
+    });
+
+    // Default task(s).
+    grunt.registerTask('default', [
+        'clean:dist',
+        'concurrent:dist',
+        'postcss',
+        'cssmin'
+    ]);
+
+};
--- README.md
+++ README.md
@@ -2,12 +2,13 @@
 :two_men_holding_hands: visualize your vk friends like social graph
 
 ## demo
-<img src="http://res.cloudinary.com/dzsjwgjii/image/upload/v1482923917/graph.png" width=900px/>
+<img src="http://res.cloudinary.com/dzsjwgjii/image/upload/v1483008450/graph2.png" width=900px/>
 
 ## install
 ```
 npm i
 node server.js
+grunt
 ```
 
 open http://local.host:3000
