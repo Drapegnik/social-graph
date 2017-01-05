4c3efffe35290cc6e57aea25ca3d29bed86893f5
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-01-05 17:46:35 +0300

    add create database method on client

--- app/home.controller.js
+++ app/home.controller.js
@@ -4,15 +4,30 @@
  * Created by Drapegnik on 22.12.16.
  */
 
-angular.module('vkGraphApp').controller('HomeCtrl', ['$location', 'Graph', 'Vk', function($location, Graph, Vk) {
-    const tokenRegEx = /access_token=(.*)&expires_in/;
-    const userIdRegEx = /&user_id=(.*)/;
+angular.module('vkGraphApp').controller('HomeCtrl', ['$scope', '$http', '$location', 'Graph', 'Vk',
+    function($scope, $http, $location, Graph, Vk) {
 
-    if ($location.$$hash.match(tokenRegEx)) {
-        var token = $location.$$hash.match(tokenRegEx)[1];
-        var userId = parseInt($location.$$hash.match(userIdRegEx)[1]);
+        const tokenRegEx = /access_token=(.*)&expires_in/;
+        const userIdRegEx = /&user_id=(.*)/;
 
-        console.log('Successfully login into vk!');
-        Vk.getGraphData(token, userId, Graph.draw);
-    }
-}]);
+        $scope.auth = false;
+
+        if ($location.$$hash.match(tokenRegEx)) {
+            $scope.token = $location.$$hash.match(tokenRegEx)[1];
+            $scope.userId = parseInt($location.$$hash.match(userIdRegEx)[1]);
+            $scope.auth = true;
+
+            console.log('Successfully login into vk!');
+            Vk.getGraphData($scope.token, $scope.userId, Graph.draw);
+        }
+
+        $scope.createDB = function() {
+            $http.get('/api/createDB', {token: $scope.token, userId: $scope.userId})
+                .then(function(response) {
+                    console.log(response);
+                })
+                .catch(function(err) {
+                    console.error(err);
+                });
+        }
+    }]);
