da5dff0b261dbec6405a7cfe9d77c828c593f735
Ivan Pazhitnykh <ivan.pazhitnykh@gmail.com>
2017-01-05 17:34:27 +0300

    refactor api methods

--- api.js
+++ api.js
@@ -13,6 +13,38 @@ var APP_ID = '5790001';
 var REDIRECT_URL = 'http://local.host:3000/home';
 var VK_API_URL = 'https://api.vk.com/method/';
 
+var _get = function(userId, token, url, method, callback) {
+    request({
+        user_ids: userId,  // jshint ignore:line
+        url: VK_API_URL + url,
+        qs: {
+            access_token: token,   // jshint ignore:line
+            v: API_VERSION,
+            fields: 'photo_50, sex, bdate'
+        },
+        method: method
+    }, callback);
+};
+
+var _getMutual = function(token, friendsIds, callback) {
+    request({
+        url: VK_API_URL + 'friends.getMutual',
+        qs: {
+            access_token: token,   // jshint ignore:line
+            v: API_VERSION,
+            target_uids: friendsIds    // jshint ignore:line
+        },
+        method: 'POST'
+    }, callback);
+};
+
+var _send = function(res, next, error, body) {
+    if (error) {
+        return next(error);
+    }
+
+    res.json(JSON.parse(body).response);
+};
 
 exports.auth = function(req, res) {
     res.redirect(url.format({
@@ -30,53 +62,15 @@ exports.auth = function(req, res) {
 };
 
 exports.getUser = function(req, res, next) {
-    request({
-        user_ids: req.body.userId,  // jshint ignore:line
-        url: VK_API_URL + 'users.get',
-        qs: {
-            access_token: req.body.token,   // jshint ignore:line
-            v: API_VERSION,
-            fields: 'photo_50, sex, bdate'
-        },
-        method: 'GET'
-    }, function(error, response, body) {
-        if (error) {
-            return next(error);
-        }
-
-        res.json(JSON.parse(body).response);
-    });
+    _get(req.body.userId, req.body.token, 'users.get', 'GET', function(e, r, b) {_send(res, next, e, b);});
 };
 
 exports.getFriends = function(req, res, next) {
-    request({
-        user_id: req.body.userId,   // jshint ignore:line
-        url: VK_API_URL + 'friends.get',
-        qs: {
-            access_token: req.body.token,   // jshint ignore:line
-            v: API_VERSION,
-            fields: 'photo_50, sex, bdate'
-        },
-        method: 'POST'
-    }, function(error, response, body) {
-        if (error) {
-            return next(error);
-        }
-
-        res.json(JSON.parse(body).response);
-    });
+    _get(req.body.userId, req.body.token, 'friends.get', 'POST', function(e, r, b) {_send(res, next, e, b);});
 };
 
 exports.getMutual = function(req, res, next) {
-    request({
-        url: VK_API_URL + 'friends.getMutual',
-        qs: {
-            access_token: req.body.token,   // jshint ignore:line
-            v: API_VERSION,
-            target_uids: req.body.friendsIds    // jshint ignore:line
-        },
-        method: 'POST'
-    }, function(error, response, body) {
+    _getMutual(req.body.token, req.body.friendsIds, function(error, response, body) {
         if (error) {
             return next(error);
         }
